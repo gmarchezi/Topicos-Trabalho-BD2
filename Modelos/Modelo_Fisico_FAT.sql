38d971ce2333a52d8a30720178dfafe7d39d831c
gmarchezi <gabriel_marchezi@hotmail.com>
2018-04-24 11:06:39 -0300

    Update Modelo_Fisico_FAT.sql

--- Modelos/Modelo_Fisico_FAT.sql
+++ Modelos/Modelo_Fisico_FAT.sql
@@ -1,33 +1,28 @@
--- Geração de Modelo físico
--- Sql ANSI 2003 - brModelo.
-
-
-
 CREATE TABLE Financiamento (
-Financiador VARCHAR(10),
-num_parcelas VARCHAR(10),
-valor_parcela VARCHAR(10),
-Valor_total VARCHAR(10),
-id_financiamento VARCHAR(10) PRIMARY KEY
+Financiador VARCHAR(30),
+num_parcelas INTEGER,
+valor_parcela FLOAT,
+Valor_total FLOAT,
+id_financiamento INTEGER PRIMARY KEY
 )
 
 CREATE TABLE Tipo_Veiculo (
-tipo_combustivel Texto(1),
-id_tipo Texto(1) PRIMARY KEY,
-nome_tipo Texto(1)
+tipo_combustivel VARCHAR(15),
+id_tipo INTEGER PRIMARY KEY,
+nome_tipo VARCHAR(15)
 )
 
 CREATE TABLE Modelo (
-altura Texto(1),
-id_modelo Texto(1) PRIMARY KEY,
-marca Texto(1),
-largura Texto(1),
-peso Texto(1),
-capacidade Texto(1),
-ano Texto(1),
-eixos Texto(1),
-nome_modelo Texto(1),
-comprimento VARCHAR(10)
+altura FLOAT,
+id_modelo INTEGER PRIMARY KEY,
+marca VARCHAR(15),
+largura FLOAT,
+peso FLOAT,
+capacidade FLOAT,
+ano INTEGER,
+eixos INTEGER,
+nome_modelo VARCHAR(15),
+comprimento FLOAT
 )
 
 CREATE TABLE Veiculo_Motorista (
@@ -37,78 +32,78 @@ id_motorista INTEGER
 )
 
 CREATE TABLE Veiculo (
-nome_veiculo Texto(1),
-placa Texto(1),
-chassi Texto(1),
+nome_veiculo VARCHAR(15),
+placa VARCHAR(10),
+chassi VARCHAR(15),
 id_veiculo INTEGER PRIMARY KEY,
-status Texto(1),
-id_modelo Texto(1),
-id_tipo Texto(1),
-id_financiamento VARCHAR(10),
-id_seguro VARCHAR(10),
+status INTEGER,
+id_modelo INTEGER,
+id_tipo INTEGER,
+id_financiamento INTEGER,
+id_seguro INTEGER,
 FOREIGN KEY(id_modelo) REFERENCES Modelo (id_modelo),
 FOREIGN KEY(id_tipo) REFERENCES Tipo_Veiculo (id_tipo),
 FOREIGN KEY(id_financiamento) REFERENCES Financiamento (id_financiamento)
 )
 
 CREATE TABLE Servico (
-data_ini Texto(1),
-valor_contrato Texto(1),
-id_servico Texto(1) PRIMARY KEY,
-data_fim Texto(1),
-status Texto(1),
-origem VARCHAR(10),
-destino VARCHAR(10),
-id_cliente Texto(1)
+data_ini DATE,
+valor_contrato FLOAT,
+id_servico INTEGER PRIMARY KEY,
+data_fim DATE,
+status INTEGER,
+origem VARCHAR(30),
+destino VARCHAR(30),
+id_cliente INTEGER
 )
 
 CREATE TABLE Funcionario (
-cargo VARCHAR(10),
-id_funcionario VARCHAR(10) PRIMARY KEY,
-id_usuario VARCHAR(10)
+cargo VARCHAR(30),
+id_funcionario INTEGER PRIMARY KEY,
+id_usuario INTEGER
 )
 
 CREATE TABLE Motorista (
-endereco Texto(1),
-status Texto(1),
-refeicao Texto(1),
-cpf Texto(1),
-salario Texto(1),
-num_contato VARCHAR(10),
+endereco VARCHAR(20),
+status INTEGER,
+refeicao FLOAT,
+cpf INTEGER,
+salario FLOAT,
+num_contato VARCHAR(18),
 id_motorista INTEGER PRIMARY KEY,
-nome Texto(1),
-id_funcionario VARCHAR(10),
+nome VARCHAR(20),
+id_funcionario INTEGER,
 FOREIGN KEY(id_funcionario) REFERENCES Funcionario (id_funcionario)
 )
 
 CREATE TABLE Seguro (
-id_seguro VARCHAR(10) PRIMARY KEY,
-Seguradora VARCHAR(10),
-Plano VARCHAR(10),
-Valor VARCHAR(10),
-Data_vencimento VARCHAR(10),
-Fim_contrato VARCHAR(10),
-status VARCHAR(10)
+id_seguro INTEGER PRIMARY KEY,
+Seguradora VARCHAR(20),
+Plano VARCHAR(20),
+Valor FLOAT,
+Data_vencimento DATE,
+Fim_contrato DATE,
+status INTEGER
 )
 
 CREATE TABLE Cliente (
-id_cliente Texto(1) PRIMARY KEY,
-id_usuario VARCHAR(10)
+id_cliente INTEGER PRIMARY KEY,
+id_usuario INTEGER
 )
 
 CREATE TABLE Usuario (
-cnpj_cpf VARCHAR(10),
-tipo_usuario VARCHAR(10),
-senha VARCHAR(10),
-id_usuario VARCHAR(10) PRIMARY KEY,
-nome VARCHAR(10),
-status VARCHAR(10),
-email VARCHAR(10)
+cnpj_cpf INTEGER,
+tipo_usuario VARCHAR(15),
+senha VARCHAR(15),
+id_usuario INTEGER PRIMARY KEY,
+nome VARCHAR(20),
+status INTEGER,
+email VARCHAR(35)
 )
 
 CREATE TABLE Veiculo_Servico (
 id_veiculo_motorista INTEGER,
-id_servico Texto(1),
+id_servico INTEGER,
 FOREIGN KEY(id_veiculo_motorista) REFERENCES Veiculo_Motorista (id_veiculo_motorista),
 FOREIGN KEY(id_servico) REFERENCES Servico (id_servico)
 )
